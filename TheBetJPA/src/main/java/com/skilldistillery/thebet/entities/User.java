2046184ba2d30c50d736b3bf86716fb3eb6088ec
Will Slaunwhite <williamslaunwhite@gmail.com>
2021-10-08 15:27:46 -0600

    Create and configure JPA project

---
+++ TheBetJPA/src/main/java/com/skilldistillery/thebet/entities/User.java
@@ -0,0 +1,72 @@
+package com.skilldistillery.thebet.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	
+	
+	
+	
+	public User() { super(); }
+
+	public User(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+
+ 
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("User [id=").append(id).append(", name=").append(name).append("]");
+		return builder.toString();
+	}
+	
+	
+}
