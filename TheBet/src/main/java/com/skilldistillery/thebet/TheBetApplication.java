661a0ffe7f8214fbcc0b7eb40dc87410619407e0
Will Slaunwhite <williamslaunwhite@gmail.com>
2021-10-08 15:46:56 -0600

    Created boot project and JPA dependency

---
+++ TheBet/src/main/java/com/skilldistillery/thebet/TheBetApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.thebet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBetApplication.class, args);
+	}
+
+}
---
+++ TheBet/src/test/java/com/skilldistillery/thebet/TheBetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.thebet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
